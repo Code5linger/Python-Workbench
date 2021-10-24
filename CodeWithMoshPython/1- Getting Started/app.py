48d82b90fb67cd9cb5007b2527167977a87c5b97
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-24 18:03:13 +0600

    MOS

--- CodeWithMoshPython/1- Getting Started/app.py
+++ CodeWithMoshPython/1- Getting Started/app.py
@@ -1,3 +1,6 @@
-print("Hello CodeSlinger!")
-print("*" * 10)
-print('Hellow Code' * 2)
+# print("Hello CodeSlinger!")
+# print("*" * 10)
+# print('Hellow Code' * 2)
+
+# print ("Hello, Code!")
+2 + 3
