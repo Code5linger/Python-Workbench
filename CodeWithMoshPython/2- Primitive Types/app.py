112888d4a071c270c09b29f0069495de562c365a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-25 17:57:15 +0600

    CWM

--- CodeWithMoshPython/2- Primitive Types/app.py
+++ CodeWithMoshPython/2- Primitive Types/app.py
@@ -1,8 +1,13 @@
-student_count = 1000
-print(student_count)
+# student_count = 1000
+# print(student_count)
 
-rating = 4.99
-print(rating)
+# rating = 4.99
+# print(rating)
 
-is_published = False
-print(is_published)
+# is_published = False
+# print(is_published)
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-2])
+print(course[0:6])
