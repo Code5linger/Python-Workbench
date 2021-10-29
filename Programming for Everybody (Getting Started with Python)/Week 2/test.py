cbde39ad9fd2a941ba6efaae7d271743dd12739a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-29 21:33:15 +0600

    Coursera

--- Programming for Everybody (Getting Started with Python)/Week 2/test.py
+++ Programming for Everybody (Getting Started with Python)/Week 2/test.py
@@ -1,1 +1,11 @@
-print('Hello form Corsera')
+# print('Hello form Corsera')
+"""
+fh = open('.\test.txt', 'r')
+
+count = 0
+for line in fh:
+    print(line.strip())
+    count = count + 1
+
+print(count, "Lines")
+"""
