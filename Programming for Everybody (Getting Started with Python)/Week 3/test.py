056105d5cd6c341a70963e49b9c1d55b8676138c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-30 13:57:45 +0600

    Coursera Week 4

--- Programming for Everybody (Getting Started with Python)/Week 3/test.py
+++ Programming for Everybody (Getting Started with Python)/Week 3/test.py
@@ -18,9 +18,15 @@ print("Hello ", name)
 #2.3 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
 
 # This first line is provided for you
-
+"""
 hrs = input("Enter Hours:")
 float_hrs = float(hrs)
 hourly_rate = input("Enter Hourly Rate:")
 float_hourly_rate = float(hourly_rate)
 print("Pay:", float_hrs * float_hourly_rate)
+"""
+
+hour = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+total_pay = float(hour) * float(rate)
+print("Pay: ", total_pay)
