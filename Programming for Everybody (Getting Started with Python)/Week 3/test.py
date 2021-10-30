23877f4b7cd85adb3607a404c0f7e7b341163199
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-30 13:45:36 +0600

    Coursera Week 4 Assesment

--- Programming for Everybody (Getting Started with Python)/Week 3/test.py
+++ Programming for Everybody (Getting Started with Python)/Week 3/test.py
@@ -1,2 +1,17 @@
+"""""
 nam = input("Who are you?")
 print("Welcome ", nam)
+"""""
+
+# Convert elevator floors
+#inp = input('Europe floor?')
+#x = 1 + 2 * 3 - 8 / 4
+# print(x)
+"""
+x = int(98.6)
+print(x)
+"""
+"""
+name = input("Name? ")
+print("Hello ", name)
+"""
