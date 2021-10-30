d9ea0147bf808737e9c056690b325ba93317c435
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-30 15:50:02 +0600

    Coursera Week 5

--- Coursera - Programming for Everybody/Week 5/test.py
+++ Coursera - Programming for Everybody/Week 5/test.py
@@ -8,3 +8,16 @@ elif x >= 2 :
 else :
     print('Big Fish')
 """
+"""
+x_raw = input("Enter a number: ")
+x = int(x_raw)
+
+if x < 2 :
+    print("Below 2")
+elif x < 20 :
+    print("Below 20")
+elif x < 10 :
+    print("Below 10")
+else :
+    print("Something Else")
+"""
