bde79d16dd31234708e9e11fe95e9693df9aa581
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-31 12:08:52 +0600

    Coursera Week 6

--- Coursera - Programming for Everybody/Week 6/test_week6.py
+++ Coursera - Programming for Everybody/Week 6/test_week6.py
@@ -15,7 +15,7 @@ print(f)
 
 print(1 + 2 * float(3) / 4 - 5)
 """
-
+"""
 x = 5
 print('Hello')
 
@@ -24,5 +24,17 @@ def print_lyrics():
     print("I sleep all night and I work all day.")
 
 print('Yo')
+print_lyrics()
 x = x + 2
 print(x)
+"""
+
+lang = input('en es fr: ')
+def greet(lang):
+    if lang == 'es':
+        print('Hola!')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+print(greet(lang))
