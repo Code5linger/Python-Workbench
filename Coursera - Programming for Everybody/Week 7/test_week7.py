dcb730a5217ccfd5cf9a3effa382dfe2a4cf206a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-10-31 14:51:56 +0600

    Coursera Week 7

--- Coursera - Programming for Everybody/Week 7/test_week7.py
+++ Coursera - Programming for Everybody/Week 7/test_week7.py
@@ -1,8 +1,19 @@
 # Loops and Iteration
-
+'''
 n = 5
 while n > 0:
     print(n)
     n = n - 1
 print(n)
 print('Blastoff!')
+'''
+
+'''
+while True:
+    line = input('>')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
+'''
+
