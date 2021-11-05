2bfe6194d2a49c195b27857403541eca24dfda56
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-05 19:53:02 +0600

    CWM 5.3

--- CodeWithMoshPython/5- Data Structures/test.py
+++ CodeWithMoshPython/5- Data Structures/test.py
@@ -1,5 +1,5 @@
 # 1
-
+"""
 letters = ['a', 'b', 'c']
 matrix = [[0, 1], [2, 3]]
 zeros = [0] * 5
@@ -7,3 +7,16 @@ combined = zeros + letters
 numbers = list(range(20))
 char = list('Hello World')
 print(len(char))
+"""
+# 2
+"""
+letters = ['a', 'b', 'c', 'd']
+letters[0] = 'Z'
+print(letters[-1])
+print(letters[0:3])
+print(letters[::2])
+"""
+"""
+numbers = list(range(20))
+print(numbers[::-1])
+"""
