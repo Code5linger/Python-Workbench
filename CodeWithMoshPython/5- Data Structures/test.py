57c9615ed2f48a378638d4b53de304cc0c3cd852
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-08 23:11:20 +0600

    CWM 5.7

--- CodeWithMoshPython/5- Data Structures/test.py
+++ CodeWithMoshPython/5- Data Structures/test.py
@@ -96,4 +96,23 @@ print(letters.count('a'))
 if 'a' in letters:
     print(letters.index('a'))
 """
+# 7
+"""
+numbers = [3, 51, 2, 8, 6]
+# numbers.sort(reverse=True)
+print(sorted(numbers, reverse=True))
+print(numbers)
+"""
+items = [
+    ('Product1', 10),
+    ('Product2', 9),
+    ('Product3', 12)
+]
 
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)
+print(items)
