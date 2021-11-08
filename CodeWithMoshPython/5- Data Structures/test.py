0f19e1f21b2d216e1b8d4465a5cc25f157f685aa
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-08 23:50:42 +0600

    CWM 5.13

--- CodeWithMoshPython/5- Data Structures/test.py
+++ CodeWithMoshPython/5- Data Structures/test.py
@@ -161,3 +161,15 @@ list2 = [10, 20, 30]
 
 print(list(zip('abc', list1, list2)))
 """
+# 13
+
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+print("redirect", browsing_session)
+
