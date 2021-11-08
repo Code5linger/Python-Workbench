91319bd45212d18d72a1747580d12b00d45ec2b6
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-09 00:48:10 +0600

    CWM 5

--- CodeWithMoshPython/5- Data Structures/test.py
+++ CodeWithMoshPython/5- Data Structures/test.py
@@ -281,7 +281,10 @@ values = list(range(5))
 values = [*first, 'a', *second, *range(5), *'Hello']
 print(values)
 """
+"""
 first = {'x': 1}
 second = {'x': 10, 'y': 2}
 combined = {**first, **second, 'z':1}
 print(combined)
+"""
+# 23
