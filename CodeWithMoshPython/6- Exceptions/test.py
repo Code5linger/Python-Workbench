7392c177d0f8c76b5995396d6c555a684bdf0743
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-09 22:46:08 +0600

    CWM 6.7

--- CodeWithMoshPython/6- Exceptions/test.py
+++ CodeWithMoshPython/6- Exceptions/test.py
@@ -73,3 +73,31 @@ try:
 except ValueError as error:
     print(error)
 """
+# 7
+from timeit import timeit
+
+code1 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError('Afawdw')
+    return 10 / age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
+"""
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+
+xfactor = calculate_xfactor(-1)
+if xfactor == None:
+    pass
+"""
+print("First Code = ", timeit(code1, number=10000))
+print("Second Code = ", timeit(code2, number=10000))
