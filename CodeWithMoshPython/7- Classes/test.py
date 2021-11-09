7ad5b08224955cc8de580fb6d21d5e1c2cf74691
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-10 04:37:38 +0600

    CWM 7.7

--- CodeWithMoshPython/7- Classes/test.py
+++ CodeWithMoshPython/7- Classes/test.py
@@ -58,7 +58,7 @@ point = Point.zero()
 point.draw()
 """
 # 6
-
+"""
 
 class Point:
     def __init__(self, x, y):
@@ -74,3 +74,22 @@ class Point:
 
 point = Point(1, 2)
 print(point)
+"""
+# 7
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
+
+point = Point(10, 20)
+other = Point(1, 2)
+print(point < other)
