4e7b9263db622edde7342289e30bc469f2aaeee4
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-09 22:19:59 +0600

    CWM  6.2

--- CodeWithMoshPython/6- Exceptions/test.py
+++ CodeWithMoshPython/6- Exceptions/test.py
@@ -3,6 +3,20 @@
 numbers = [1, 2]
 print(numbers[3])
 """
+"""
 age = int(input('Age: '))
 
 print(age)
+"""
+# 2
+"""
+try:
+    age = int(input('Age: '))
+    print('You are ', age, ' Years Old')
+except ValueError:
+    print('Enter a number between 1 to 100. '
+          'Unless you are Keanu Reeves')
+else:
+    print('Boring!')
+print('Something')
+"""
