6536b6cf3947b8a182fe12ced09215d49a65824f
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-10 04:40:29 +0600

    CWM 7.8

--- CodeWithMoshPython/7- Classes/test.py
+++ CodeWithMoshPython/7- Classes/test.py
@@ -76,7 +76,7 @@ point = Point(1, 2)
 print(point)
 """
 # 7
-
+"""
 
 class Point:
     def __init__(self, x, y):
@@ -93,3 +93,20 @@ class Point:
 point = Point(10, 20)
 other = Point(1, 2)
 print(point < other)
+"""
+# 8
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
+point = Point(10, 20)
+other = Point(1, 2)
+combined = point + other
+print(combined.x)
