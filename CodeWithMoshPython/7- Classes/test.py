0e6136285c755e6fee35a7b7f7b9b48ea65d7246
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-10 03:58:03 +0600

    CWM 7.3

--- CodeWithMoshPython/7- Classes/test.py
+++ CodeWithMoshPython/7- Classes/test.py
@@ -13,3 +13,20 @@ point = Point()
 print(type(point))
 print(isinstance(point, Point))
 """
+# 3
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        print(f'Point ({self.x}, {self.y})')
+
+
+point = Point(1, 2)
+point.draw()
+
+another = Point(3, 4)
+another.draw()
