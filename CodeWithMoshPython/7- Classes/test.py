aec1853bc88c586383f6a4ef332175d6f2506b74
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-10 05:08:55 +0600

    CWM 7.9

--- CodeWithMoshPython/7- Classes/test.py
+++ CodeWithMoshPython/7- Classes/test.py
@@ -95,7 +95,7 @@ other = Point(1, 2)
 print(point < other)
 """
 # 8
-
+"""
 
 class Point:
     def __init__(self, x, y):
@@ -110,3 +110,34 @@ point = Point(10, 20)
 other = Point(1, 2)
 combined = point + other
 print(combined.x)
+"""
+# 9
+
+
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+
+cloud = TagCloud()
+cloud['python'] = 10
+len(cloud)
+cloud.add('Python')
+cloud.add('python')
+cloud.add('python')
+print(cloud.tags)
