1e92e8cc78800c895851251e772283c8bc5f3e68
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-10 04:07:47 +0600

    CWM 7.5

--- CodeWithMoshPython/7- Classes/test.py
+++ CodeWithMoshPython/7- Classes/test.py
@@ -14,7 +14,7 @@ print(type(point))
 print(isinstance(point, Point))
 """
 # 3
-
+"""
 
 class Point:
     default_color = 'red'
@@ -35,3 +35,24 @@ point.draw()
 
 another = Point(3, 4)
 another.draw()
+"""
+# 5
+
+
+class Point:
+    default_color = 'red'
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
+
+    def draw(self):
+        print(f'Point ({self.x}, {self.y})')
+
+
+point = Point.zero()
+point.draw()
