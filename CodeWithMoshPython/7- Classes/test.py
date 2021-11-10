49d356463227de96659d42d68ea52dc9087a7ef1
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-10 17:02:26 +0600

    CWM 7.14

--- CodeWithMoshPython/7- Classes/test.py
+++ CodeWithMoshPython/7- Classes/test.py
@@ -216,7 +216,7 @@ m.eat()
 print(m.age)
 """
 # 13
-
+"""
 
 class Animal:
     def __init__(self):
@@ -236,3 +236,37 @@ class Mammal(Animal):
 m = Mammal()
 print(isinstance(m, object))
 print(issubclass(Mammal, object))
+"""
+# 14
+"""
+
+class Animal:
+    def __init__(self):
+        print('Animal Cons')
+        self.age = 1
+
+    def eat(self):
+        print('eat')
+
+
+# Animal: Parent, Base
+# Mammal: Child, Sub
+class Mammal(Animal):
+    def __init__(self):
+        print('Mammal Cons')
+        self.weight = 2
+        super().__init__()
+
+    def walk(self):
+        print('walk')
+
+
+class Fish(Animal):
+    def swim(self):
+        print('swim')
+
+
+m = Mammal()
+print(m.age)
+print(m.weight)
+"""
