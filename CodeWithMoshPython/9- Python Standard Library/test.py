c9595bc81351b27c12fcdbeaa14fb6db0665fc43
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-11 18:40:26 +0600

    CWM 9.4

--- CodeWithMoshPython/9- Python Standard Library/test.py
+++ CodeWithMoshPython/9- Python Standard Library/test.py
@@ -7,7 +7,7 @@ Path('')
 
 """
 # 3
-""""""
+"""
 from pathlib import Path
 
 path = Path('../8- Modules/')
@@ -15,4 +15,15 @@ path = Path('../8- Modules/')
 paths = [p for p in path.iterdir()]
 path.glob("*.py")
 print(paths)
+"""
+# 4
+"""
+from pathlib import Path
+from time import ctime
 
+path = Path('../8- Modules/ecommerce/__init__.py')
+path.exists()
+path.rename('init.txt')
+path.unlink()
+path.stat()
+"""
