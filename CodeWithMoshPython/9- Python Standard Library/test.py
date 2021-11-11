71c856708e8d80ef3bc19712a9abed5151f54349
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-11 18:45:13 +0600

    CWM 9.5

--- CodeWithMoshPython/9- Python Standard Library/test.py
+++ CodeWithMoshPython/9- Python Standard Library/test.py
@@ -27,3 +27,17 @@ path.rename('init.txt')
 path.unlink()
 path.stat()
 """
+# 5
+'''
+from pathlib import Path
+from zipfile import ZipFile
+"""
+with ZipFile('files.zip', 'w') as zip:
+    for path in Path('ecommerce').rglob('*.*'):
+        zip.write(path)
+        
+zip.close()
+"""
+with ZipFile(files.zip) as zip:
+    print(zip.namelist())
+'''
